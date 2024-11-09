e23d198a835ccb16c62f699ca4c6af4cce7f42be
QuvonchbekCoder05 <166432175+QuvonchbekCoder05@users.noreply.github.com>
2024-11-09 19:09:43 +0500

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import TaskAPIView, SpecialTaskAPIView
 
 urlpatterns = [
-    
+
     # Task API yo'llari
-    path('tasks/', TaskAPIView.as_view(), name='task_list_create'),  # GET va POST uchun
-    path('tasks/<int:pk>/', TaskAPIView.as_view(), name='task_update_delete'),  # PUT va DELETE uchun
+    path('tasks/', TaskAPIView.as_view(), name='tasks_list_create'),  # GET va POST uchun api qilish qismi
+    path('tasks-id/<int:pk>/', TaskAPIView.as_view(), name='tasks_update_delete'),  # PUT va DELETE uchun api qsimi
 
     # SpecialTask API yo'llari
-    path('special-tasks/', SpecialTaskAPIView.as_view(), name='special_task_list_create'),  # GET va POST uchun
-    path('special-tasks/<int:pk>/', SpecialTaskAPIView.as_view(), name='special_task_update_delete'),  # PUT va DELETE uchun
+    path('special-tasks/', SpecialTaskAPIView.as_view(), name='special_tasks_list_create'),   # GET va POST uchun api qilish qismi
+    path('special-tasks-id/<int:pk>/', SpecialTaskAPIView.as_view(), name='special_tasks_update_delete'),   # PUT va DELETE uchun api qsimi
 ]
