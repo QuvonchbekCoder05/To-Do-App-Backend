11e638c91b1478cacc19ef44fe9768e635ad3ef6
QuvonchbekCoder05 <166432175+QuvonchbekCoder05@users.noreply.github.com>
2024-11-12 21:21:21 +0500

    Update models.py
    manager va qoshimcha funksiyalar alohgida yozildi

--- accounts/models.py
+++ accounts/models.py
@@ -1,24 +1,12 @@
 # This code defines a custom user model with a custom user manager, along with an OTP model for
 # generating and validating one-time passwords.
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.utils import timezone
+from .managers import CustomUserManager #managaerdan CustomManagerni chqirib oldim
+from .utils import is_otp_valid # utilsdan qoshimcha maydonni chqirib oldim
 
-class CustomUserManager(BaseUserManager):# Foydalanuvchilar modelini boshqarish.
-    def create_user(self, email, username, password=None, **other_fields):
-        if not email:
-            raise ValueError("Provide email") # # Emailning mavjudligini tekshirib olamiz 
-        email = self.normalize_email(email) ## Email manzilini normallashtirish uchun normileze metodina foydalanamiz 
-        user = self.model(email=email, username=username, **other_fields)
-        user.set_password(password) # Parolni Django'ning hashlash funksiyasi yordamida o'rnatib olamiz 
-        user.save()
-        return user
 
-    def create_superuser(self, email, username, password=None, **other_fields): #Admin huquqlariga ega superfoydalanuvchini yaratish
-        other_fields.setdefault('is_staff', True) # Superuser uchun is_staff ni True ga o'rnatib qoyamiz 
-        other_fields.setdefault('is_superuser', True)  # Superuser uchun is_superuserni ham truega ornatib qoyamiz admin bolishi uchun
-        other_fields.setdefault('is_active', True)# Superuser  faolligini ta'minlashni amslhs odhiramiz 
-        return self.create_user(email, username, password, **other_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin): #  AbstractBaseUser va PermissionsMixin'dan meros oladigan moslashtirilgan foydalanuvchi modelini yaratib olamiz 
     email = models.EmailField(unique=True)
@@ -42,8 +30,7 @@ class OTP(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
 
-    def is_valid(self):
-        return timezone.now() <= self.expires_at # Hozirgi vaqt tugash vaqtidan oldin yoki teng bo'lsa True qaytaradigan qilib qoyamiz
+
 
     def __str__(self):
         return f"OTP for {self.user.email} - {self.otp_code}"
